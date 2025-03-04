07e2c075731801cdc66fac2eb040900009991a8e
Raphael Rodrigues Rosa <85590638+rapharosa@users.noreply.github.com>
2025-03-04 18:31:49 -0300

    Add files via upload

--- estudos-pyton/exercicios/ex004.py
+++ estudos-pyton/exercicios/ex004.py
@@ -1,2 +1,12 @@
+# Faça um programa que leia algo pelo teclado, e mostre na tela o seu tipo primitivo, e 
+# todas as informações possiveis sobre ele
+
 d = input('Digite algo: ')
-print('o que foi digitado é do tipo ',type(d))
+print('o que foi digitado é do tipo ', type(d))
+print('Só tem espaços? ', d.isspace())
+print('É numerico? ', d.isnumeric())
+print('É alfabetico? ', d.isalpha())
+print('É alfanumerico? ', d.isalnum())
+print('Está em maiusculas? ', d.isupper())
+print('Está em minusculas? ', d.islower())
+print('Está capitalizada? ', d.istitle())
