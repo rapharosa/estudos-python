01f1d2de666f46dbf039bbd743c793c8a0a080a6
Raphael Rodrigues Rosa <85590638+rapharosa@users.noreply.github.com>
2025-03-05 21:15:25 -0300

    Add files via upload

--- estudos-pyton/exercicios/ex010.py
+++ estudos-pyton/exercicios/ex010.py
@@ -1,5 +1,5 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dolares ela pode comprar
-# considere 1 dolar 3,27 reais
+# considere 1 dolar 5,79 reais
 
 n = int (input('Quantos reais você tem? '))
-print('Com esse valor você consegue comprar {:.2f} dolares'.format(n/3.27))
+print('Com esse valor você consegue comprar {:.2f} dolares'.format(n/5.79))
