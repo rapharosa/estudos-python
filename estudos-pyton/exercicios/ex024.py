8301410096108daafddf0880384c45c646c2fd7e
Raphael Rodrigues Rosa <85590638+rapharosa@users.noreply.github.com>
2025-03-12 21:04:40 -0300

    Add files via upload

--- estudos-pyton/exercicios/ex024.py
+++ estudos-pyton/exercicios/ex024.py
@@ -1,7 +1,6 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou nao com o nome "santo"
 cidade = str(input('Digite o nome de uma cidade: '))
 
-tratada = cidade.title().strip()
+tratada = cidade[:5].title().strip()
 
 print('Santo' in tratada)
-print(tratada)
