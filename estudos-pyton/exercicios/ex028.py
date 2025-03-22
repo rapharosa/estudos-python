d696fd7db0628255cd24ac9427d430ecdbf88251
Raphael Rodrigues Rosa <85590638+rapharosa@users.noreply.github.com>
2025-03-21 21:24:58 -0300

    Add files via upload

--- estudos-pyton/exercicios/ex028.py
+++ estudos-pyton/exercicios/ex028.py
@@ -1,8 +1,11 @@
 #Escreva um programa que faça o computador pensar um numero inteiro entre 0 e 5 e peça para o usuario descobrir qual numero
 #foi escolhido pelo computador. O programa deverá se o usuario venceu ou perdeu
 import random
+from time import sleep
 numero = random.randrange(0, 5)
 
-resposta = int(input('Pensei em um numero de 0 a 5, advinhe qual foi... '))
+print('Vou pensar em um numero...')
+sleep(2)
+resposta = int(input('Advinhe qual foi... '))
 
 print('Acertou miseravi' if numero == resposta else 'errrrrroooouuu')
